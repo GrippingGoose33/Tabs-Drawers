82eb87b1247e2170acc1d628cb2be6bb4dde168c
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-05-16 13:02:00 -0500

    Tabs 1

--- App.js
+++ App.js
@@ -9,7 +9,11 @@ const Tab =createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-
+      <Tab.Navigator>
+        <Tab.Screen name = "Home" component={HomeScreen}/>
+        <Tab.Screen name = "News" component={NewsScreen}/>
+        <Tab.Screen name = "Settings" component={SettingsScreen}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
